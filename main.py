a8d6bb8e5a276f97627d189f2eb6a30ffccce037
Zhengming Ding <allanzmding@gmail.com>
2024-03-17 09:38:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -27,12 +27,3 @@ def fast_align_MED(S, T, MED={}):
     # TODO - keep track of alignment
     pass
 
-def test_MED():
-    for S, T in test_cases:
-        assert fast_MED(S, T) == MED(S, T)
-                                 
-def test_align():
-    for i in range(len(test_cases)):
-        S, T = test_cases[i]
-        align_S, align_T = fast_align_MED(S, T)
-        assert (align_S == alignments[i][0] and align_T == alignments[i][1])
